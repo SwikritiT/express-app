1c2b45281708b89787167124b8eec875275484aa
Swikriti Tripathi <swikriti808@gmail.com>
2024-06-14 11:50:46 +0545

    change message to test again
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World! How you doin?</h1>')
+app.get("/", (req, res) => {
+	res.send("<h1>Hello World! How you doin?*joey's voice*</h1>")
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
